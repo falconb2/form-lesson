d4b0c30fa53804d449e0f06cc97010390344992e
falconA7 <kaweco17@gmail.com>
2026-02-15 02:24:11 +0900

    Add form submission handling to contact form

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+const form = document.getElementById("contactForm")//contactFormを操作する//
+
+form.addEventListener("submit",function(e){
+    e.preventDefault();
+
+    const name= document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+
+    if(name === ""|| email === "" ||message === ""){
+        alert("すべて入力してください");
+        return;
+    }
+
+    console.log("送信データ");
+    console.log(name,email,message);
+
+    alert("送信されました");
+
+
+
+})
+
+
 
